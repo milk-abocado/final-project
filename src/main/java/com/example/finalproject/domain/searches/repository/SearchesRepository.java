177e20a4dd951e11edaa71e64e2b2d87091c9633
yeonju <qoduswn9810@gmail.com>
2025-09-08 20:45:02 +0900

    add: Repository 추가 - 특정 사용자 검색 기록 조회 및 특정 검색어 기록 조회

--- src/main/java/com/example/finalproject/domain/searches/repository/SearchesRepository.java
+++ src/main/java/com/example/finalproject/domain/searches/repository/SearchesRepository.java
@@ -3,8 +3,13 @@ package com.example.finalproject.domain.searches.repository;
 import com.example.finalproject.domain.searches.entity.Searches;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SearchesRepository extends JpaRepository<Searches,Long> {
-    Optional<Searches> findByUserIdAandKeywordAndRegion(Long userId, String keyword, String region);
+    Optional<Searches> findByUserIdAndKeywordAndRegion(Long userId, String keyword, String region);
+
+    List<Searches> findByUserId(Long userId);
+
+    List<Searches> findByUserIdAndRegion(Long userId, String region);
 }
