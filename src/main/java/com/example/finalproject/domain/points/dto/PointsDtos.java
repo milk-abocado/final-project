9495ad86519b587a575f08e438bb0da09b5822e5
soobinny <sbbin22@gmail.com>
2025-09-10 18:30:26 +0900

    feat: PointsDtos 추가 - request: 적립/사용 - response: 기록/잔액,내역

---
+++ src/main/java/com/example/finalproject/domain/points/dto/PointsDtos.java
@@ -0,0 +1,46 @@
+package com.example.finalproject.domain.points.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PointsDtos {
+
+    //포인트 적립
+    @Getter
+    @Setter
+    public static class EarnRequest{
+        private Long userId;
+        private int amount;
+        private String reason;
+    }
+
+    //포인트 사용 요청
+    @Getter
+    @Setter
+    public static class UserRequest{
+        private Long userId;
+        private Long orderId; // 추후 주문 구현 시 DB 추가해야함
+        private int amount;
+    }
+
+    // 포인트 기록
+    @Getter
+    @Setter
+    public static class PointResponse{
+        private Long pointId;
+        private Long userId;
+        private int amount;
+        private String reason;
+        private LocalDateTime createdAt;
+    }
+
+    //포인트 잔액, 내역
+    public static class UserPointsResponse{
+        private Long userId;
+        private int totalPoints;
+        private List<PointResponse> history;
+    }
+}
