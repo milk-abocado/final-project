effff692473ab1c25b98b769e0ff8a51856b0757
soobinny <sbbin22@gmail.com>
2025-09-10 18:48:53 +0900

    fix: PointsRepository 제네릭 수정

--- src/main/java/com/example/finalproject/domain/points/repository/PointsRepository.java
+++ src/main/java/com/example/finalproject/domain/points/repository/PointsRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PointsRepository extends JpaRepository {
+public interface PointsRepository extends JpaRepository<Points, Long> {
 
     List<Points> findByUserId(Long userId);
 }
