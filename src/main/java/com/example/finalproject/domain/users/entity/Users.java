842ec794fba51e4d25f3e4e9feee50f49d9d0fc8
yeonju <qoduswn9810@gmail.com>
2025-09-11 17:07:16 +0900

    add: Users

---
+++ src/main/java/com/example/finalproject/domain/users/entity/Users.java
@@ -0,0 +1,38 @@
+package com.example.finalproject.domain.users.entity;
+
+
+import com.example.finalproject.domain.users.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity @Table(name = "users")
+public class Users {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    private String nickname;
+    private boolean deleted;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist void onCreate(){ createdAt = updatedAt = LocalDateTime.now(); }
+    @PreUpdate void onUpdate(){ updatedAt = LocalDateTime.now(); }
+
+    public Object getUsername() {
+        return null;
+    }
+}
