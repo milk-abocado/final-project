a1d344ce990077e8cf57ff808066e27ad4d86287
dori135 <nolli77@ewha.ac.kr>
2025-09-11 15:42:45 +0900

    🔥 delete: 중복 파일 삭제

--- src/main/java/com/example/finalproject/domain/stores/repository/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.finalproject.domain.stores.repository;
-
-import com.example.finalproject.domain.stores.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-    // 현재는 기본 CRUD 메서드만 사용
-}
