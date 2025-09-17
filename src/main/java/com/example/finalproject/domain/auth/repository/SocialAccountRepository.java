91a0aa38092a8e4d7865d404da9141cc9b075906
이연우 <yeunwoo217@gmail.com>
2025-09-17 15:59:06 +0900

    📝 chore: 프로그램 실행에 필요한 주석 해제

--- src/main/java/com/example/finalproject/domain/auth/repository/SocialAccountRepository.java
+++ src/main/java/com/example/finalproject/domain/auth/repository/SocialAccountRepository.java
@@ -1,12 +1,12 @@
-//package com.example.finalproject.domain.auth.repository;
-//
-//import com.example.finalproject.domain.auth.entity.SocialAccount;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
-//    boolean existsByProviderAndProviderId(String provider, String providerId);
-//    Optional<SocialAccount> findByProviderAndProviderId(String provider, String providerId);
-//    Optional<SocialAccount> findByUserIdAndProvider(Long userId, String provider);
-//}
+package com.example.finalproject.domain.auth.repository;
+
+import com.example.finalproject.domain.auth.entity.SocialAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
+    boolean existsByProviderAndProviderId(String provider, String providerId);
+    Optional<SocialAccount> findByProviderAndProviderId(String provider, String providerId);
+    Optional<SocialAccount> findByUserIdAndProvider(Long userId, String provider);
+}
