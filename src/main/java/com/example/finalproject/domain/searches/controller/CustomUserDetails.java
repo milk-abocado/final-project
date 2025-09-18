f3e0fb7d2705de71efbd661fa03e348c989cdf14
yeonju <qoduswn9810@gmail.com>
2025-09-18 13:45:16 +0900

    fix: searches 오류 수정

--- src/main/java/com/example/finalproject/domain/searches/controller/CustomUserDetails.java
+++ src/main/java/com/example/finalproject/domain/searches/controller/CustomUserDetails.java
@@ -1,4 +1,9 @@
 package com.example.finalproject.domain.searches.controller;
 
 public class CustomUserDetails {
+    public Long getUserId;
+
+    public Long getUserId() {
+        return 0L;
+    }
 }
