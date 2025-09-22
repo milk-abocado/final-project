4ed1acbda0a6a9f40b19ca0c484b26f157ccdf50
yeonju <qoduswn9810@gmail.com>
2025-09-23 05:34:55 +0900

    feat: elasticsearch repository

---
+++ src/main/java/com/example/finalproject/domain/elasticsearchpopular/repository/PopularSearchRepository.java
@@ -0,0 +1,12 @@
+package com.example.finalproject.domain.elasticsearchpopular.repository;
+
+import com.example.finalproject.domain.elasticsearchpopular.entity.PopularSearch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PopularSearchRepository extends JpaRepository<PopularSearch, Long> {
+    List<PopularSearch> findTop10ByCount(int count);
+}
