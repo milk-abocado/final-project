96c1b420c2b5edc4d99fb541bee887916b17a0d0
dori135 <nolli77@ewha.ac.kr>
2025-09-23 12:25:33 +0900

    ✨ feat: 메뉴 카테고리 및 옵션 CUD 기능 구현 - 메뉴 카테고리 단건 추가/수정 기능 구현 - 메뉴 카테고리 단건/전체 삭제 리팩토링 - 메뉴 옵션 그룹 생성 기능 구현 - 메뉴 옵션 그룹 단건 삭제 리팩토링 - 메뉴 옵션 선택지 단건 추가/수정/삭제 기능 구현 - 중복 코드 제거

--- src/main/java/com/example/finalproject/domain/menus/dto/request/MenuOptionsRequest.java
+++ src/main/java/com/example/finalproject/domain/menus/dto/request/MenuOptionsRequest.java
@@ -1,12 +1,14 @@
 package com.example.finalproject.domain.menus.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class MenuOptionsRequest {
     private String optionsName;
     private Boolean isRequired;
