e7ebe2b8bc60947d6321d60541ba39b2bc45e0f6
yeonju <qoduswn9810@gmail.com>
2025-09-23 14:51:56 +0900

    feat: ps controller 지역별 인기 검색어 조회

--- src/main/java/com/example/finalproject/domain/elasticsearchpopular/controller/PopularSearchController.java
+++ src/main/java/com/example/finalproject/domain/elasticsearchpopular/controller/PopularSearchController.java
@@ -5,6 +5,7 @@ import com.example.finalproject.domain.elasticsearchpopular.repository.PopularSe
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,8 +17,12 @@ public class PopularSearchController {
 
     private final PopularSearchRepository popularSearchRepository;
 
+    //region 파라미터 없으면 전국 인기검색어 반환
     @GetMapping
-    public List<PopularSearch> getPopularSearches() {
+    public List<PopularSearch> getPopularSearches(@RequestParam(required = false) Integer page, @RequestParam(required = false) String region) {
+        if (region != null) {
+            return popularSearchRepository.findTop10ByRegionOrderByRankAsc(region);
+        }
         return popularSearchRepository.findTop10ByOrderByRankAsc();
     }
 }
