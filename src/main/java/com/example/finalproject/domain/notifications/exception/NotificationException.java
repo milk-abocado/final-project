795e64e50fff0e5646b58170a98e8cc6a2b6e71f
soobinny <sbbin22@gmail.com>
2025-09-29 17:47:39 +0900

    ♻️ refactor: notification exception 리팩토링 및 파일 추가
    - NotificationErrorCodeErrorCode: 에러코드 상세 구현
    - NotificationExceptionHandler 추가
    - SlackService 예외 발생 시 코드 수정

--- src/main/java/com/example/finalproject/domain/notifications/exception/NotificationException.java
+++ src/main/java/com/example/finalproject/domain/notifications/exception/NotificationException.java
@@ -1,7 +1,14 @@
 package com.example.finalproject.domain.notifications.exception;
 
 public class NotificationException extends RuntimeException {
-    public NotificationException(String message) { super(message); }
-    public NotificationException(String message, Throwable cause) { super(message, cause); }
-}
+    private final NotificationErrorCode errorCode;
 
+    public NotificationException(NotificationErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public NotificationErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
